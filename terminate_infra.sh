b4c213a4ccf884d22ab871c6a05dc802aad51ad1
Benoit Delbosc <bdelbosc@nuxeo.com>
2018-01-24 12:35:05 +0100

    Add a confirmation on terminate so no one will drop all its work by mistake :/

--- terminate_infra.sh
+++ terminate_infra.sh
@@ -10,7 +10,20 @@ function terminate() {
   popd
 }
 
+confirm() {
+    # call with a prompt string or use a default
+    read -r -p "${1:-Are you sure? [y/N]} " response
+    case "$response" in
+        [yY][eE][sS]|[yY]) 
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+
 # ------------------------------
 # main
-terminate
+confirm && terminate
 
